38a6172d6c26d60ea4e3444e097c1fc6fe5b281f
Samuel Barabas <samuel@owee.de>
2018-07-20 21:03:21 +0200

    refactoring; submit to prometheus push gateway

--- src/roomclimatesensor.ino
+++ src/roomclimatesensor.ino
@@ -6,121 +6,136 @@
 DHT dht(DHTPIN, DHTTYPE);
 ADC_MODE(ADC_VCC);
 
-void setup(void) {
-
+struct dhtdata {
     float temperature;
     float humidity;
     float heatindex;
     float vcc;
+    String mac;
+};
 
-    Serial.begin(115200);
-    Serial.println();
-
-    WiFi.begin(SSID, PASS);
-
-    dht.begin();
+dhtdata measurement(void) {
     // wait a bit before read values, otherwise it will fail often.
     // TODO: figure out minimum sleep time
-    delay(500);
-    temperature = dht.readTemperature();
-    humidity = dht.readHumidity();
+    //delay(2000);
 
-    // currenlty it says always 2.77 and I'm not sure if this is working
-    // correclty. I never tried with batterie power yet.
-    // I saw somwhere, that for precice measurements a capacitator is needed
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+
+	if (isnan(temperature) || isnan(humidity)) {
+        Serial.println("reading DHT failed");
+    }
+
+    // Currenlty it says always 2.77 V when for VCC and I'm not sure if this
+    // is working correclty. I never tried with battery power yet.
+    // I saw somwhere that for precice measurements a capacitator is needed
     // somewhere, not sure if NodeMCU and WeMos D1 mini have this already and
     // if it's really needed.
-    vcc = ESP.getVcc() / 1000.0;
+    float vcc = ESP.getVcc() / 1000.0;
 
-    if (!isnan(temperature) && !isnan(humidity)) {
-        heatindex = dht.computeHeatIndex(temperature, humidity, false);
+    dhtdata m = {
+        temperature,
+        humidity,
+        dht.computeHeatIndex(temperature, humidity, false),
+		vcc,
+        WiFi.macAddress()
+    };
+    return m;
+}
 
-        Serial.println("temperature: " + String(temperature) + " C");
-        Serial.println("humidity: " + String(humidity) + " %");
-        Serial.println("heat index: " + String(heatindex) + " C");
-        Serial.println("vcc: " + String(vcc) + " V");
+bool waitforwifi(void) {
+    Serial.print("waiting for wifi");
 
-        Serial.print("waiting for wifi");
-        int waitloops = 0;
-        while (WiFi.status() != WL_CONNECTED && waitloops < 20) {
-            Serial.print(".");
-            delay(500);
-            waitloops++;
-        }
+    for (int i = 0; i < 20; i++) {
+        Serial.print(".");
         if (WiFi.status() == WL_CONNECTED) {
+            Serial.print("success");
+
             // I have the suspicion that WL_CONNECTED doesn't mean that we already got
             // a IP from the DHCP server. Better wait a bit here.
             // TODO: check if I right with my suspicion and implement a wait-loop
             //       for the ip address if needed.
             delay(3000);
-
             Serial.print(" address is ");
             Serial.println(WiFi.localIP());
-
-            WiFiClient client;
-            if (client.connect(PRTG_HOST, PRTG_PORT)) {
-                Serial.print("connected to prtg, sending data... ");
-
-                String url = "/" + String(PRTG_SENSOR) + "?content=" +
-                    "<prtg>" +
-                    "<result>" +
-                      "<channel>Temperature</channel>" +
-                      "<unit>Celsius</unit>" +
-                      "<float>1</float>" +
-                      "<value>" + String(temperature) + "</value>" +
-                    "</result>" +
-                    "<result>" +
-                      "<channel>Humidity</channel>" +
-                      "<unit>Percent</unit>" +
-                      "<float>1</float>" +
-                      "<value>" + String(humidity) + "</value>" +
-                    "</result>" +
-                    "<result>" +
-                      "<channel>HeatIndex</channel>" +
-                      "<unit>Celsius</unit>" +
-                      "<float>1</float>" +
-                      "<value>" + String(heatindex) + "</value>" +
-                    "</result>" +
-                    "<result>" +
-                      "<channel>Voltage</channel>" +
-                      "<unit>Volt</unit>" +
-                      "<float>1</float>" +
-                      "<value>" + String(vcc) + "</value>" +
-                    "</result>" +
-                    "</prtg>";
-
-                client.print("GET " + url + " HTTP/1.1\r\n" +
-                    "Host: " + PRTG_HOST + "\r\n" +
-                    "Connection: close\r\n\r\n");
-
-                while(client.available()) {
-                    client.readStringUntil('\r');
-                }
-
-                client.stop();
-
-                Serial.println("done");
-            } else {
-                Serial.println("connection to prtg failed");
-            }
-        } else {
-            Serial.println(" unable to connect to wifi");
+            return true;
         }
-    } else {
-        Serial.println("reading DHT failed");
+        delay(500);
     }
 
+    // fail if not successfull after couple of seconds
+    Serial.println("failed");
+    return false;
+}
+
+bool turnoffwifi(void) {
     // don't know if this is needed. need to check the power consumption.
     Serial.println("turning off WiFi");
     WiFi.disconnect();
     WiFi.mode(WIFI_OFF);
+}
 
+void showdata(dhtdata* data) {
+    Serial.println("temperature: " + String(data->temperature) + " C");
+    Serial.println("humidity: " + String(data->humidity) + " %");
+    Serial.println("heat index: " + String(data->heatindex) + " C");
+    Serial.println("vcc: " + String(data->vcc) + " V");
+    Serial.println("mac address: " + String(data->mac));
+}
+
+bool submit(dhtdata* data) {
+    WiFiClient client;
+    if (client.connect(PUSHGATEWAY_HOST, PUSHGATEWAY_PORT)) {
+        Serial.print("connected to " + String(PUSHGATEWAY_HOST) + ", sending data... ");
+
+        String body = "esp_temperature{esp_id=\"" + data->mac + "\"} " + String(data->temperature) + "\n"
+            + "esp_humidity{esp_id=\"" + data->mac + "\"} " + String(data->humidity) + "\n"
+            + "esp_heatindex{esp_id=\"" + data->mac + "\"} " + String(data->heatindex) + "\n"
+            + "esp_vcc{esp_id=\"" + data->mac + "\"} " + String(data->vcc) + "\n";
+
+        client.println("POST /metrics/job/esp HTTP/1.1\r\n"
+            "Host: " + String(PUSHGATEWAY_HOST) + ":" + PUSHGATEWAY_PORT + "\r\n"
+            "User-Agent: d1mini/010\r\n"
+            "Accept: */*\r\n"
+            "Content-Type: application/x-www-form-urlencoded\r\n"
+            "Content-length: " + body.length() + "\r\n"
+            "\r\n" + body);
+
+        while(client.available()) {
+            client.read();
+        }
+
+        client.stop();
+
+        Serial.println("done");
+		return true;
+	}
+    Serial.println("connection to pushgateway failed");
+	return false;
+}
+
+void deepsleep(void) {
     // TODO: what is the difference betwee WAKE_RF_DISABLED and without it?
     Serial.println("going to deep sleep for " + String(SLEEP) + " seconds");
     ESP.deepSleep(SLEEP * 1000000);
     //ESP.deepSleep(SLEEP * 1000000, WAKE_RF_DISABLED);
 }
 
+void setup(void) {
+    Serial.begin(115200);
+    Serial.println();
+
+    WiFi.begin(SSID, PASS);
+    dht.begin();
+	waitforwifi();
+}
+
+
 void loop(void) {
-    // not used
+	dhtdata data = measurement();
+    showdata(&data);
+	submit(&data);
+	turnoffwifi();
+	deepsleep();
+    delay(SLEEP * 1000);
 }
